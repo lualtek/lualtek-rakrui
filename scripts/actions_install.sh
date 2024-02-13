d7a6f2e920afed4bd5c3565d2582957ccf833cc7
LasaleFamine <info@godev.space>
2024-02-13 15:33:50 +0100

    chore: ci removing clang 8

--- scripts/actions_install.sh
+++ scripts/actions_install.sh
@@ -4,11 +4,6 @@ set -e
 
 pip3 install clint pyserial setuptools adafruit-nrfutil
 sudo apt-get update
-sudo apt-get install -y --allow-downgrades libllvm8 -V
-sudo apt install -fy cppcheck clang-format-8
-if [ ! -f /usr/bin/clang-format ]; then
-    sudo ln -s /usr/bin/clang-format-8 /usr/bin/clang-format
-fi
 
 # make all our directories we need for files and libraries
 mkdir ${HOME}/.arduino15
