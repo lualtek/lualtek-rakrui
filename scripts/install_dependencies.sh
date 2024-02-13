8d4223c722486ce4f2275165778c53119cba075d
LasaleFamine <info@godev.space>
2024-02-13 13:31:11 +0100

    chore: ci install deps

---
+++ scripts/install_dependencies.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+# Install dependencies needed for examples
+
+# 1. Install dependencies from Arduino Library Manager
+arduino-library manager install Adafruit_Sensor
+arduino-library manager install Tiny_BME280
+
